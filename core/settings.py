a416a31bc20a4616fdb1c83da75a64ca6e6a9e8c
Ricky James Aguilar <rickyjamesaguilar@gmail.com>
2019-03-02 13:26:31 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aquinasdb',
+        'NAME': 'aquinas_prod',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost'
